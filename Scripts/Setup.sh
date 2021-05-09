76ca5ceb06f6289ae81cad0400270e539f13f466
Garvit Joshi <55401175+garvit-joshi@users.noreply.github.com>
2021-05-10 01:51:51 +0530

    Updated Manjaro Setup Script, Added VS Code Service Menu File

--- Scripts/Setup.sh
+++ Scripts/Setup.sh
@@ -8,10 +8,12 @@ sudo pacman -S --needed base-devel
 sudo pacman -S yay llvm clang lldb php
 sudo pacman -S jre-openjdk-headless jre-openjdk jdk-openjdk openjdk-doc openjdk-src
 yay -S visual-studio-code-bin
+sudo cp assets/openinvscode.desktop /usr/share/kservices5/ServiceMenus/
 yay -S lldb-mi-git
 yay -S teamviewer
 yay -S google-chrome
 git config --global user.name "Garvit Joshi"
 git config --global user.email "55401175+garvit-joshi@users.noreply.github.com"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo snap refresh
 echo "Done"
