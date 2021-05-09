cccf1494b849618b202a427125c9e70c84289d61
Garvit Joshi <55401175+garvit-joshi@users.noreply.github.com>
2021-05-10 01:37:45 +0530

    Added Manjaro Setup Script

---
+++ Scripts/Setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+# Setup Manjaro Script
+echo "Manjaro Setup"
+sudo pacman -Syu
+sudo pacman -S --needed base-devel
+sudo pacman -S yay llvm clang lldb php
+sudo pacman -S jre-openjdk-headless jre-openjdk jdk-openjdk openjdk-doc openjdk-src
+yay -S visual-studio-code-bin
+yay -S lldb-mi-git
+yay -S teamviewer
+yay -S google-chrome
+git config --global user.name "Garvit Joshi"
+git config --global user.email "55401175+garvit-joshi@users.noreply.github.com"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Done"
