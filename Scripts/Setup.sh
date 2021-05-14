256501c86c2f44c78aa5b6062e38780c9764b221
Garvit Joshi <garvitjoshi9@gmail.com>
2021-05-15 01:03:54 +0530

    updated setup script

--- Scripts/Setup.sh
+++ Scripts/Setup.sh
@@ -7,13 +7,14 @@ sudo pacman -Syu
 sudo pacman -S --needed base-devel
 sudo pacman -S yay llvm clang lldb php gcc
 sudo pacman -S jre-openjdk-headless jre-openjdk jdk-openjdk openjdk-doc openjdk-src
-yay -S visual-studio-code-bin
 sudo cp assets/openinvscode.desktop /usr/share/kservices5/ServiceMenus/
 yay -S lldb-mi-git
 yay -S teamviewer
 yay -S google-chrome
 git config --global user.name "Garvit Joshi"
-git config --global user.email "55401175+garvit-joshi@users.noreply.github.com"
+git config --global user.email "garvitjoshi9@gmail.com"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sudo snap refresh
+sudo snap install telegram-desktop
+sudo snap install code --classic
 echo "Done"
