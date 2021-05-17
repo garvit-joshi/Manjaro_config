565b7c02832c6153292757a3a825262293504b82
Garvit Joshi <garvitjoshi9@gmail.com>
2021-05-17 07:23:14 +0530

    Updated Script

--- Scripts/Setup.sh
+++ Scripts/Setup.sh
@@ -5,18 +5,32 @@
 echo "Manjaro Setup"
 sudo pacman -Syu
 sudo pacman -S --needed base-devel
-sudo pacman -S yay llvm clang lldb php gcc
+sudo pacman -S yay llvm clang lldb php gcc qt6-base libsecret seahorse
 sudo pacman -S jre-openjdk-headless jre-openjdk jdk-openjdk openjdk-doc openjdk-src
 sudo cp assets/openinvscode.desktop /usr/share/kservices5/ServiceMenus/
 yay -S lldb-mi-git
 yay -S teamviewer
 yay -S google-chrome
+
+# git confiigure
 git config --global user.name "Garvit Joshi"
 git config --global user.email "garvitjoshi9@gmail.com"
+
+# omz and powerline
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-# Nerd Font: https://www.nerdfonts.com/font-downloads
+
+# Pyenv: Python Environment
+echo "Set Pyenv: https://github.com/pyenv/pyenv" 
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+cd ~/.pyenv && src/configure && make -C src
+
+# Nerd Font
+echo "Nerd Font: https://www.nerdfonts.com/font-downloads"
+
+# snap
 sudo snap refresh
 sudo snap install telegram-desktop
 sudo snap install code --classic
