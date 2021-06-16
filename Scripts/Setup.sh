74c9e833f8a2fa206ac7ef97ac4799dfb94c678d
Garvit Joshi <garvitjoshi9@gmail.com>
2021-06-16 19:25:11 +0530

    Updated Script

--- Scripts/Setup.sh
+++ Scripts/Setup.sh
@@ -4,14 +4,15 @@
 # Setup Manjaro Script
 echo "Manjaro Setup"
 sudo pacman-mirrors --api --set-branch unstable
-sudo pacman -Syu
+sudo pacman-mirrors --fasttrack && sudo pacman -Syyu
 sudo pacman -S --needed base-devel
-sudo pacman -S yay llvm clang lldb php gcc qt6-base libsecret seahorse
+sudo pacman -S yay llvm clang lldb php gcc qt6-base libsecret seahorse cmake
 sudo pacman -S jre-openjdk-headless jre-openjdk jdk-openjdk openjdk-doc openjdk-src
+sudo pacman -S noto-fonts-emoji
 yay -S lldb-mi-git
-yay -S teamviewer
 yay -S google-chrome
 yay -S visual-studio-code-bin
+yay -S neovim-nightly-bin
 sudo cp assets/openinvscode.desktop /usr/share/kservices5/ServiceMenus/
 
 # git confiigure
@@ -21,15 +22,15 @@ git config --global user.email "garvitjoshi9@gmail.com"
 # omz, powerline and nvm
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 
-# Pyenv: Python Environment
+# Pyenv: Python Environment, NVM
 echo "Set Pyenv: https://github.com/pyenv/pyenv" 
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 cd ~/.pyenv && src/configure && make -C src
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
 # Nerd Font
 echo "Nerd Font: https://www.nerdfonts.com/font-downloads"
