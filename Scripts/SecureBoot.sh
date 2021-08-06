a278715c3408d51827af7a4b767a94cac06d6da3
Garvit Joshi <garvitjoshi9@gmail.com>
2021-08-07 00:19:59 +0530

    Add files via upload

---
+++ Scripts/SecureBoot.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+# Setup Secure Boot:
+# 1. change /boot/efi in /etc/fstab to just /efi
+# 2. create /efi with sudo mkdir /efi
+# 3. re-mount your efi partition with sudo mount -a
+mkdir -p -v /etc/efikeys
+chmod -v 700 /etc/efikeys
+cd /etc/efikeys
+efi-readvar -v PK -o old_PK.esl
+efi-readvar -v KEK -o old_KEK.esl
+efi-readvar -v db -o old_db.esl
+efi-readvar -v dbx -o old_dbx.esl
+openssl req -new -x509 -newkey rsa:2048 -subj "/CN=Garvit's platform key/" -keyout PK.key -out PK.crt -days 3650 -nodes -sha256
+openssl req -new -x509 -newkey rsa:2048 -subj "/CN=Garvit's key-exchange-key/" -keyout KEK.key -out KEK.crt -days 3650 -nodes -sha256
+openssl req -new -x509 -newkey rsa:2048 -subj "/CN=Garvit's kernel-signing key/" -keyout db.key -out db.crt -days 3650 -nodes -sha256
+cert-to-efi-sig-list -g "$(uuidgen)" PK.crt PK.esl
+sign-efi-sig-list -k PK.key -c PK.crt PK PK.esl PK.auth
+cert-to-efi-sig-list -g "$(uuidgen)" KEK.crt KEK.esl
+sign-efi-sig-list -a -k PK.key -c PK.crt KEK KEK.esl KEK.auth
+cert-to-efi-sig-list -g "$(uuidgen)" db.crt db.esl
+sign-efi-sig-list -a -k KEK.key -c KEK.crt db db.esl db.auth
+sign-efi-sig-list -k KEK.key -c KEK.crt dbx old_dbx.esl old_dbx.auth
+openssl x509 -outform DER -in PK.crt -out PK.cer
+openssl x509 -outform DER -in KEK.crt -out KEK.cer
+openssl x509 -outform DER -in db.crt -out db.cer
+cat old_KEK.esl KEK.esl > compound_KEK.esl
+cat old_db.esl db.esl > compound_db.esl
+sign-efi-sig-list -k PK.key -c PK.crt KEK compound_KEK.esl compound_KEK.auth
+sign-efi-sig-list -k KEK.key -c KEK.crt db compound_db.esl compound_db.auth
