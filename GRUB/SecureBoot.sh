ceccfc256914007b4e0f1af507597d8c4300d877
Garvit Joshi <garvitjoshi9@gmail.com>
2021-08-18 20:51:00 +0530

    Updated Secure Boot Setup

--- GRUB/SecureBoot.sh
+++ GRUB/SecureBoot.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# sudo su
-# chmod +x SecureBoot.sh
+# List of Commands to enable Secure boot on Manjaro
 # Link: https://wiki.gentoo.org/wiki/User:Sakaki/Sakaki%27s_EFI_Install_Guide/Configuring_Secure_Boot#Saving_Current_Keystore_Values.2C_and_Creating_New_Keys
+sudo pacman -S efitools
+curl https://github.com/andreyv.gpg | gpg --import
+pamac build sbupdate
+sudo su   #Enter Superuser
 mkdir -p -v /etc/efikeys
 chmod -v 700 /etc/efikeys
 cd /etc/efikeys
@@ -28,3 +31,5 @@ cat old_db.esl db.esl > compound_db.esl
 sign-efi-sig-list -k PK.key -c PK.crt KEK compound_KEK.esl compound_KEK.auth
 sign-efi-sig-list -k KEK.key -c KEK.crt db compound_db.esl compound_db.auth
 cp -v *.{auth,cer,crt,esl} /boot/efi/
+exit # Exit Superuser
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Manjaro --modules="normal test efi_gop efi_uga search echo linux all_video gfxmenu gfxterm_background gfxterm_menu gfxterm loadenv configfile tpm" --disable-shim-lock
